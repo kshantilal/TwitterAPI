a7fdffb29e57b8b420f6b4f4a9bc2f1e03b8bb32
Kunal Shantilal <845870587@ken20-202-08.local>
2017-08-17 10:05:47 +1200

    deleted gitignore

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
+var twit = require('twit');
 
+
+var T = new Twit({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+})
 
 app.use(cors());
 
